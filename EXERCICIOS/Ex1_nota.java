af7fb290a52e1cc366b38cb24f0658b09f5045c3
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-02-23 17:22:24 -0300

    Delete Ex1_nota.java

--- EXERCICIOS/Ex1_nota.java
+++
@@ -1,5 +0,0 @@
-/*
- * Faça um programa que leia o conjunto de 2 valores,
- * o primeiro representando o nome do aluno e o segundo,
- * representando a sua idade. [pare o prorgrama inserindo o valor 0 no campo nome]
- */
