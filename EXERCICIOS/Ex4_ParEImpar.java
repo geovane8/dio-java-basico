e1de04e46ddeedb6a8962e0e2d8c7ac28f395596
geovane8 <geovane.n7@gmail.com git config --global user.email geovane.n7@gmail.com git config --global user.email geovane.n7@gmail.com>
2023-02-23 21:55:20 -0300

    finalizado exercicio 4

--- EXERCICIOS/Ex4_ParEImpar.java
+++ EXERCICIOS/Ex4_ParEImpar.java
@@ -7,7 +7,7 @@ e a quantidade de números impares.
 */
 
 public class Ex4_ParEImpar {
-    
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -15,21 +15,24 @@ public class Ex4_ParEImpar {
         int numero;
         int quantPares = 0, quantImpares = 0;
 
-        System.out.println("Quantidade de números: ");
+        System.out.println("\nQuantidade de números: ");
         quantNumeros = scan.nextInt();
 
         int count = 0;
+
         do {
-            System.out.println("Número: ");
+            System.out.println("\nNúmero: ");
             numero = scan.nextInt();
 
             if (numero % 2 == 0 ) quantPares++;
             else quantImpares++;
 
             count++;
-        } while(count < quantNumeros);
+        } while(count < quantNumeros); 
 
-        System.out.println("Quantidade Par: " + quantPares);
+              
+
+        System.out.println("\r\nQuantidade Par: " + quantPares);
         System.out.println("Quantidade Ímpar: " + quantImpares);
     }
 }
